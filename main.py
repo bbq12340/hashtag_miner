8e887173b710ca0f6e13fdae7b09341aba32b6d5
bbq12340 <bbq12340@hotmail.com>
2020-08-05 22:03:03 +0900

    not user

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from miner import Miner
+import pandas as pd
 
-miner = Miner("programming")
+miner = Miner("맛집")
+
+df = pd.read_csv("hashtag.csv")
+
+tags = list(df.index.values)
+
+time = df.columns[len(df.columns)-1]
+counts = list(df[time].values)
+
+mean = miner.analyze("hashtag.csv")
+
+DF = pd.DataFrame({
+    'hashtag': tags,
+    'current posts': counts,
+    'average increase': mean,
+})
+DF.to_csv('result.csv')
+
--- miner.py
+++ miner.py
@@ -26,13 +26,31 @@ class Miner:
         tag_list = search_tag(self.tag)
         data = mine_tag(tag_list)
         self.realtime_logging("Hashtag Mining Completed!")
-        counts = get_counts(data["hashtag"])
         for i in range(0, self.sets):
+            counts = get_counts(data["hashtag"])
             data = data_frame(data, counts)
             data.to_csv('hashtag.csv')
-            self.realtime_logging(f"{i}/{self.sets} completed!")
+            self.realtime_logging(f"{i+1}/{self.sets} completed!")
             time.sleep(60*self.intervals)
+        df = data.set_index('hashtag')
         now = datetime.now()
         date = now.strftime("%m/%d/%Y, %H:%M:%S")
         with open('activity.txt', 'a') as f:
             f.write(f"{date} | Session Finished\n")
+        return df
+    
+    def analyze(self, filename):
+        df = pd.read_csv(filename)
+        target_data = {"hastag": list(df.index.values)}
+        analysis_df = pd.DataFrame(target_data)
+        for n in range(1, len(df.columns)):
+            former = df.columns[n]
+            latter = df.columns[n+1]
+            diff = df[latter] - df[former]
+            analysis_df[f'diff{n}'] = diff
+        analysis_df = analysis_df.set_index("hashtag")
+        analysis_df['mean'] = analysis_df.mean(axis=1)
+        return analysis_df
+
+    
+
