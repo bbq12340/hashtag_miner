2a39ab6f8427e91bdcff336041994c166f12c124
bbq12340 <bbq12340@hotmail.com>
2020-08-11 18:29:07 +0900

    fix analyze

--- miner.py
+++ miner.py
@@ -1,65 +1,56 @@
-from tag_mining import (
-    search_tag,
-    mine_tag,
-    get_counts,
-    data_frame,
-)
+from tag_mining import mine_tag, get_counts
 from datetime import datetime
 import time
 import pandas as pd
 import numpy as np
 
 class Miner:
-    def __init__(self, tag, intervals=60, sets=3):
+    def __init__(self, tag, custom, intervals=60, sets=3):
         self.tag = tag
+        self.custom = custom
         self.intervals = intervals
         self.sets = sets
-        self.realtime_logging("Hashtag Miner Started")
-        self.df = self.start_miner()
+        self.activity_log("Miner Activated",3)
 
-    
-    def get_time(self):
+    def activity_log(self, text: str, datetype: int):
         now = datetime.now()
-        time = now.strftime("%H:%M:%S")
-        return time
-    
-    def realtime_logging(self, text):
-        time = self.get_time()
+        if datetype == 1:
+            time = now.strftime("%d/%m/%Y")
+        elif datetype == 2:
+            time = now.strftime("%H:%M:%S")
+        else:
+            time = now.strftime("%d/%m/%Y, %H:%M:%S")
         with open('activity.txt', 'a') as f:
-            f.write(f"{time}  -------------> {text}\n")
+            f.write(f"{time} --------> {text}\n")
 
     def start_miner(self):
-        tag_list = mine_tag(self.tag) #list
-        self.realtime_logging("Hashtag Mining Completed!")
-
-        data = {'hashtag': tag_list}
-        df = pd.DataFrame(data) #DF
-
-        for i in range(0, self.sets):
-            time = self.get_time()
-            counts = get_counts(tag_list) #list
-            df.insert(i+1, time, counts)
-            df.to_csv('hashtag.csv')
-            self.realtime_logging(f"{i+1}/{self.sets} completed!")
+        mine_tag(self.tag, self.custom)
+        for attempt in range(self.sets):
+            get_counts(f"{self.tag}.csv")
             time.sleep(60*self.intervals)
-        with open('activity.txt', 'a') as f:
-            f.write("Session Finished\n")
-        return df
+            self.activity_log(f"{attempt+1}/{self.sets} sets completed",2)
+        self.activity_log("Mining Completed!", 3)
+
+    def analyze(self, filename):
+        df = pd.read_csv(filename, index_col=0)
+        df.set_index('hashtags', inplace=True)
+        df = df.transpose()
+        
+        Data = {}
+
+
     
-def analyze(filename):
-    df = pd.read_csv(filename)
-    data = {}
-    for i in range(1, len(df.columns)):
-        data[f'inc-rate{i}'] = df[df.columns.values[i+1]] / df[df.columns.values[i]]
-    preview = pd.DataFrame(data, index=df["hashtag"])
-    rank_df = preview.apply(np.argsort, axis=1)
-    ranked_cols = df.columns.to_series()[rank_df.values[:,::-1][:,:2]]
-    data = {
-        'latests': df[df.columns[-1]],
-        'hot-time': ranked_cols[0], #highest increase interval
-        '2nd hot-time': ranked_cols[1], #2nd highest
-        'mean': preview.mean(axis=1) #mean of every difference
-    }
-    df_analysis = pd.DataFrame(data, index=df['hashtag'])
-    df_analysis.to_csv("analysis.csv")
+        for i in range(1, len(df.columns)):
+            data[f'inc-rate{i}'] = df[df.columns.values[i+1]] / df[df.columns.values[i]]
+        preview = pd.DataFrame(data, index=df["hashtag"])
+        rank_df = preview.apply(np.argsort, axis=1)
+        ranked_cols = df.columns.to_series()[rank_df.values[:,::-1][:,:2]]
+        data = {
+            'latests': df[df.columns[-1]],
+            'hot-time': ranked_cols[0], #highest increase interval
+            '2nd hot-time': ranked_cols[1], #2nd highest
+            'mean': preview.mean(axis=1) #mean of every difference
+        }
+        df_analysis = pd.DataFrame(data, index=df['hashtag'])
+        df_analysis.to_csv(f"{filename}-analysis.csv")
 
--- tag_mining.py
+++ tag_mining.py
@@ -1,82 +1,122 @@
-import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from collections import OrderedDict
 import time
 from datetime import datetime
-import csv
 import pandas as pd
 
-INSTAGRAM_QUERY = "https://www.instagram.com/explore/tags/"
 HASHTAG_INFO = By.CLASS_NAME, "WSpok"
 POST_COUNT = "g47SY"
 RELATED_TAGS = "LFGs8"
+rel_tags = '//*[@id="ajax"]/div/div/div[1]'
+rel_tags_by = By.XPATH, rel_tags
+
+def instagram_refresh(browser):
+    for attempt in range(0, 10):
+        try:
+            print("trying"+attempt)
+            browser.sleep(3)
+            browser.refresh()
+            wait = WebDriverWait(browser, 10)
+            wait.until(EC.presence_of_element_located(HASHTAG_INFO))
+            post_count = browser.find_element_by_class_name(POST_COUNT).get_attribute("innerText")
+        except TimeoutException:
+            continue
+    post_count = "TIMEOUT"
+    return post_count
 
 
-def open_browser(q):
+def open_browser():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')  
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-    driver.get(INSTAGRAM_QUERY + q)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.presence_of_element_located(HASHTAG_INFO))
     return driver
 
-def data_frame(data, counts):
-    now = datetime.now()
-    time = now.strftime("%H:%M:%S")
-    df = pd.DataFrame(data, index=None)
-    df[f"{time}"] = counts
-    return df
+def open_instagram(driver, q):
+    driver.get(f"https://www.instagram.com/explore/tags/{q}/")
+    wait = WebDriverWait(driver, 10)
+    try:
+        wait.until(EC.presence_of_element_located(HASHTAG_INFO))
+    except TimeoutException:
+        instagram_refresh(driver)
+    return driver
 
-def get_counts(tag_list):
-    COUNT = []
-    for t in tag_list:
-        browser = open_browser(t)
-        while True:
-            try:
-                post_count = browser.find_element_by_class_name(POST_COUNT).get_attribute("innerText")
-                browser.close()
-            except TimeoutException:
-                continue
-            break
-        COUNT.append(post_count)
-    return COUNT
-
-def mine_tag(tag):
+def open_tagfinder(driver, q, custom):
     TAGS = []
-    TAGS.append(tag)
-    print(len(TAGS))
-    while len(TAGS) < 100:
+    driver.get(f"https://www.tagsfinder.com/ko-kr/?hashtag={q}&limit=30&country=kr&fs=off&fp=off&fg=off&custom={custom}&type=mix")    
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.presence_of_element_located(rel_tags_by))
+    rel = driver.find_element_by_xpath(rel_tags)
+    li = rel.find_elements_by_tag_name("span")
+    for t in li:
+        t = t.get_attribute('innerText').strip('#').strip('\nX')
+        TAGS.append(t)  
+    return TAGS
+
+def mine_tag(parent_tag: str, custom=""):
+    HASHTAGS = []
+    HASHTAGS.append(parent_tag)
+    browser = open_browser()
+    while len(HASHTAGS) < 100:
         for t in range(0,100):
             while True:
                 try:
-                    print(t)
-                    related_tag_list = search_tag(TAGS[t])
-                    TAGS.extend(related_tag_list)
-                    TAGS = list(OrderedDict.fromkeys(TAGS))
-                    print(related_tag_list)
-                    print(len(TAGS))
+                    related_tag_list = open_tagfinder(browser, HASHTAGS[t], custom)
+                    HASHTAGS.extend(related_tag_list)
+                    HASHTAGS = list(OrderedDict.fromkeys(HASHTAGS))
                 except TimeoutException:
                     continue
                 break       
-            if len(TAGS) >= 100:
+            if len(HASHTAGS) >= 100:
                 break
-    return TAGS
+    data = {'hashtags': HASHTAGS}
+    df = pd.DataFrame(data)
+    df.to_csv(f"{parent_tag}.csv")
+    return df
 
-def search_tag(tag):
-    REL_TAGS = []
-    browser = open_browser(tag)
-    rel_tags = browser.find_elements_by_class_name(RELATED_TAGS)
+def get_counts(filename):
+    COUNT = []
+    df = pd.read_csv(filename, index_col=0)
+    tag_list = list(df['hashtags']) 
+    browser = open_browser()
+    #count for each hashtags
+    for t in tag_list:
+        print(tag_list.index(t), t)
+        browser = open_instagram(browser, t)
+        try:
+            post_count = browser.find_element_by_class_name(POST_COUNT).get_attribute("innerText")
+        except TimeoutException:
+            instagram_refresh(browser)
+        except NoSuchElementException:
+            try:
+                banned = browser.find_element_by_class_name("_4Kbb_")
+                post_count = "banned"
+            except NoSuchElementException:
+                posts = browser.find_elements_by_class_name("v1Nh3")
+                post_count = len(posts)
+        COUNT.append(post_count)
+    #clean count-list
+    CLEANED_VALUE = []
+    for elem in COUNT:
+        if type(elem) == str:
+            try:
+                value = int(elem.replace(",",""))
+                CLEANED_VALUE.append(value)
+            except ValueError:
+                value = elem
+                CLEANED_VALUE.append(value)
+        else:
+            CLEANED_VALUE.append(elem)
+    COUNT = CLEANED_VALUE
 
-    for tag in rel_tags:
-        tag = tag.get_attribute('innerText').strip('#')
-        REL_TAGS.append(tag)
-    browser.close()
-    return REL_TAGS
+    now = datetime.now()
+    time = now.strftime("%H:%M:%S")
+    df.insert(len(df.columns), time, COUNT)
+    df.to_csv(filename, index=False)
+    return COUNT
