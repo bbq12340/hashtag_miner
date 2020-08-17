13ac4c1676ebadfd4df7e72291195e0036689bed
bbq12340 <bbq12340@hotmail.com>
2020-08-17 13:31:24 +0900

    tkinter

--- main.py
+++ main.py
@@ -1,57 +1,63 @@
-from miner import Miner, analyze
+from miner import Miner
 import pandas as pd
 import tkinter as tk
 
 
 root = tk.Tk()
 root.title("Hashtag Miner")
-root.geometry("400x400")
+root.geometry("350x250")
 
 #variable
 tag = tk.StringVar()
+custom = tk.StringVar()
+num = tk.IntVar()
 interval = tk.IntVar()
 sets = tk.IntVar()
 
 #function
 def start_mining():
-    search_button.grid_forget()
-    analyze_button.grid_forget()
-    mining_loading_label.grid(row=3, column=0, columnspan=2, pady=5)
-    Miner(hashtag_entry.get(), intervals=interval_entry.get(), sets=set_entry.get())
-def start_analyze():
-    search_button.grid_forget()
-    analyze_button.grid_forget()
-    analyze_loading_label.grid(row=3, column=0, columnspan=2, pady=5)
-    analyze("hashtag.csv")
+    miner = Miner(tag.get(), custom.get(), num.get(), interval.get(), sets.get())
+    miner.start_miner()
+    miner.start_analyzer()
 
 #label
 hashtag_label = tk.Label(root, text="# 해시태그: ")
 hashtag_label.grid(row=0, column=0, padx=5, pady=5)
 
+custom_label = tk.Label(root, text="제외 키워드: ")
+custom_label.grid(row=1, column=0, padx=5, pady=5)
+
+num_label = tk.Label(root, text="수량: ")
+num_label.grid(row=2, column=0, padx=5, pady=5)
+
 interval_label = tk.Label(root, text="시간: ")
-interval_label.grid(row=1, column=0, padx=5, pady=5)
+interval_label.grid(row=3, column=0, padx=5, pady=5)
 
 set_label = tk.Label(root, text="세트: ")
-set_label.grid(row=2, column=0, padx=5, pady=5)
+set_label.grid(row=4, column=0, padx=5, pady=5)
 
 mining_loading_label = tk.Label(root, text="마이닝 중...")
 analyze_loading_label = tk.Label(root, text="분석 중...")
 
 #entry
-hashtag_entry = tk.Entry(root, textvariable=tag)
+hashtag_entry = tk.Entry(root, width=10, textvariable=tag)
 hashtag_entry.grid(row=0, column=1, pady=5)
 
-interval_entry = tk.Entry(root, textvariable=interval)
-interval_entry.grid(row=1, column=1, pady=5)
+custom_entry = tk.Entry(root, width=10, textvariable=custom)
+custom_entry.grid(row=1, column=1, pady=5)
 
-set_entry = tk.Entry(root, textvariable=sets)
-set_entry.grid(row=2, column=1, pady=5)
+num_entry = tk.Entry(root, width=10, textvariable=num)
+num_entry.grid(row=2, column=1, pady=5)
+
+interval_entry = tk.Entry(root, width=10, textvariable=interval)
+interval_entry.grid(row=3, column=1, pady=5)
+
+set_entry = tk.Entry(root, width=10, textvariable=sets)
+set_entry.grid(row=4, column=1, pady=5)
 
 #button
-search_button = tk.Button(root, text="마이닝 시작", command=start_mining)
-search_button.grid(row=3, column=0, columnspan=2, pady=5)
+search_button = tk.Button(root, text="Activate", command=start_mining)
+search_button.grid(row=5, column=2, columnspan=2, padx=5, pady=5)
 
-analyze_button = tk.Button(root, text="분석 시작", command=start_analyze)
-analyze_button.grid(row=4, column=0, columnspan=2, pady=5)
 
 root.mainloop()
