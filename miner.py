d5477023bfba42f6ee4ccaf13bfa7a87bb45eeca
bbq12340 <bbq12340@hotmail.com>
2020-08-24 22:47:17 +0900

    2020/08/24 update

--- miner.py
+++ miner.py
@@ -1,41 +1,77 @@
-from tag_mining import mine_tag, get_counts, analyze
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
+import requests
+from bs4 import BeautifulSoup
+import time
 from datetime import datetime
-import time, schedule
 import pandas as pd
 import numpy as np
+import random
 
-class Miner:
-    def __init__(self, tag, custom, num, intervals=2, sets=3):
+class TagMiner:
+    def __init__(self, tag, custom=None, num=30):
         self.tag = tag
         self.custom = custom
         self.num = num
-        self.intervals = intervals
-        self.sets = sets
-        self.activity_log("Miner Activated",3)
-        mine_tag(self.tag, self.custom,self.num)
+        self.browser = self.open_browser()
+        HASHTAGS_DF = pd.DataFrame({'hashtags': self.search_mediance()})
+        print(HASHTAGS_DF['hashtags'])
 
-    def activity_log(self, text: str, datetype: int):
-        now = datetime.now()
-        if datetype == 1:
-            time = now.strftime("%d/%m/%Y")
-        elif datetype == 2:
-            time = now.strftime("%H:%M:%S")
-        else:
-            time = now.strftime("%d/%m/%Y, %H:%M:%S")
-        with open('activity.txt', 'a') as f:
-            f.write(f"{time} --------> {text}\n")
+        while len(HASHTAGS_DF['hashtags']) < self.num:
+            random_tags = random.sample(list(HASHTAGS_DF['hashtags']), 10)
+            NEW_DF = self.search_tagsfinder(random_tags)
+            HASHTAGS_DF = pd.concat([HASHTAGS_DF, NEW_DF], ignore_index=True)
+            HASHTAGS_DF.drop_duplicates(subset=['hashtags'])
+            if len(HASHTAGS_DF['hashtags']) >= self.num:
+                self.browser.close()
+                HASHTAGS_DF.to_csv(f'{self.tag}.csv', encoding='utf-8')
+                break
+    def open_browser(self):
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.wait = WebDriverWait(browser, 30)
+        return browser
+    def search_mediance(self):
+        SIMILAR_RANK = 'rank_table'
+        SIMILAR_RANK_BY_ID = By.CLASS_NAME, SIMILAR_RANK
+        NO_RESULT = 'nosearch_con'
+        HASHTAGS = []
+        self.browser.get(f"http://tag.mediance.co.kr/analytics/tag/{self.tag}")
+        try:
+            self.wait.until(EC.presence_of_element_located(SIMILAR_RANK_BY_ID))
+            html = self.browser.execute_script('return document.documentElement.outerHTML')
+            soup = BeautifulSoup(html, 'html.parser')
+            similar_rank_table = soup.find('table', {'class': 'rank_table'})
+            table_row = similar_rank_table.find_all('tr')
+            table_row.pop(0)
+            for row in table_row:
+                hashtag = row.find('a').text
+                HASHTAGS.append(hashtag)
+            return HASHTAGS
+        except (TimeoutException, NoSuchElementException):
+            self.browser.find_element_by_class_name(NO_RESULT)
+            HASHTAGS = []
+            return HASHTAGS
+    def search_tagsfinder(self, hashtags):
+        CLIPBOARD = 'clipboard'
+        CLIPBOARD_BY_ID = By.ID, CLIPBOARD
+        HASHTAGS = []
+        query_tags = ('%2C%20').join(hashtags)
+        self.browser.get(f"https://www.tagsfinder.com/ko-kr/?hashtag={query_tags}&limit=30&country=kr&fs=off&fp=off&fg=off&custom={self.custom}&type=mix")
+        self.wait.until(EC.presence_of_element_located(CLIPBOARD_BY_ID))
+        html = self.browser.execute_script('return document.documentElement.outerHTML')
+        soup = BeautifulSoup(html, 'html.parser')
+        tags = soup.find_all('span', {'class': 'tag'})
+        for tag in tags:
+            tag = tag.text[1:].split('X')[0]
+            HASHTAGS.append(tag)
+        hashtag_df = pd.DataFrame({'hashtags': HASHTAGS})
+        return hashtag_df
 
-    def start_miner(self):
-        for attempt in range(self.sets):
-            schedule.every(self.intervals).hours.do(get_counts(f"{self.tag}.csv"))
-            self.activity_log(f"{attempt+1}/{self.sets} sets completed",2)
-        self.activity_log("Mining Completed!", 3)
-        df = pd.read_csv(f"{self.tag}.csv", index_col=0)
-        df.set_index('hashtags', inplace=True)
-        df = df.transpose()
-        df.to_csv(f"{self.tag}_analysis.csv", index=False)
+
+        
     
-    def start_analyzer(self):
-        analyze(f"{self.tag}_analysis.csv")
-
-
